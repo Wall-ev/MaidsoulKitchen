6c1f56c3262f63159501e44771ed8c2174f344f8
Azumic <775300932@qq.com>
2024-11-15 22:00:54 +0800

    fix

--- src/main/java/com/github/catbert/tlma/item/bauble/BurnProtectBauble.java
+++ src/main/java/com/github/catbert/tlma/item/bauble/BurnProtectBauble.java
@@ -3,27 +3,18 @@ package com.github.catbert.tlma.item.bauble;
 import com.github.catbert.tlma.api.ILittleMaidBauble;
 import com.github.catbert.tlma.data.ModDamageTypeTags;
 import com.github.catbert.tlma.init.InitEffects;
-import com.github.catbert.tlma.init.InitItems;
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidAttackEvent;
 import com.github.tartaricacid.touhoulittlemaid.api.event.MaidDamageEvent;
 import com.github.tartaricacid.touhoulittlemaid.entity.item.EntityExtinguishingAgent;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.util.ItemsUtil;
-import net.minecraft.resources.ResourceKey;
 import net.minecraft.world.damagesource.DamageSource;
-import net.minecraft.world.damagesource.DamageType;
 import net.minecraft.world.effect.MobEffectInstance;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
-import net.minecraftforge.registries.RegistryObject;
-import org.jetbrains.annotations.NotNull;
 
-import java.util.List;
-
-import static com.github.catbert.tlma.item.bauble.BurnDamageManager.getBurnDamageTypes;
 
 public class BurnProtectBauble implements ILittleMaidBauble {
 
@@ -35,7 +26,6 @@ public class BurnProtectBauble implements ILittleMaidBauble {
     public void onLivingDamage(MaidDamageEvent event) {
         EntityMaid maid = event.getMaid();
         DamageSource source = event.getSource();
-        List<ResourceKey<DamageType>> fireDamageTypes = getBurnDamageTypes();
         if (source.is(ModDamageTypeTags.DAMAGES_BURN)) {
             int slot = ItemsUtil.getBaubleSlotInMaid(maid, this);
             if (slot >= 0) {
@@ -55,7 +45,6 @@ public class BurnProtectBauble implements ILittleMaidBauble {
     public void onBurnDamage(MaidAttackEvent event) {
         EntityMaid maid = event.getMaid();
         DamageSource source = event.getSource();
-        List<ResourceKey<DamageType>> burnDamageTypes = getBurnDamageTypes();
         if (maid.hasEffect(InitEffects.BURN_PROTECT.get()) && source.is(ModDamageTypeTags.DAMAGES_BURN)) {
             event.setCanceled(true);
         }
