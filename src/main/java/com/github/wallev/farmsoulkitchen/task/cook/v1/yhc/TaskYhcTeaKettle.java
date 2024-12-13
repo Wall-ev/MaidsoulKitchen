d14f2329902de845c44303b8d8980f9df134001b
Azumic <775300932@qq.com>
2024-12-13 15:23:28 +0800

    fix yhs teakettle water logic

--- src/main/java/com/github/wallev/farmsoulkitchen/task/cook/v1/yhc/TaskYhcTeaKettle.java
+++ src/main/java/com/github/wallev/farmsoulkitchen/task/cook/v1/yhc/TaskYhcTeaKettle.java
@@ -11,6 +11,7 @@ import com.github.wallev.farmsoulkitchen.util.FakePlayerUtil;
 import com.github.tartaricacid.touhoulittlemaid.api.entity.data.TaskDataKey;
 import com.github.tartaricacid.touhoulittlemaid.entity.passive.EntityMaid;
 import com.github.tartaricacid.touhoulittlemaid.util.ItemsUtil;
+import com.mojang.datafixers.util.Pair;
 import dev.xkmc.youkaishomecoming.content.pot.kettle.KettleBlockEntity;
 import dev.xkmc.youkaishomecoming.content.pot.kettle.KettleRecipe;
 import dev.xkmc.youkaishomecoming.init.registrate.YHBlocks;
@@ -29,10 +30,12 @@ import net.minecraftforge.common.util.Lazy;
 import net.minecraftforge.items.ItemHandlerHelper;
 import net.minecraftforge.items.ItemStackHandler;
 import net.minecraftforge.items.wrapper.CombinedInvWrapper;
+import net.minecraftforge.items.wrapper.RecipeWrapper;
 
 import java.lang.ref.WeakReference;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 
@@ -109,12 +112,58 @@ public class TaskYhcTeaKettle extends TaskFdPot<KettleBlockEntity, KettleRecipe>
 
     @Override
     public boolean shouldMoveTo(ServerLevel level, EntityMaid maid, KettleBlockEntity be, MaidRecipesManager<KettleRecipe> manager) {
-        return super.shouldMoveTo(level, maid, be, manager) || (needWater(be) && findMaidHasWaterResource(maid, be) != -1);
+        return super.shouldMoveTo(level, maid, be, manager);
     }
 
     @Override
     public void processCookMake(ServerLevel level, EntityMaid maid, KettleBlockEntity be, MaidRecipesManager<KettleRecipe> manager) {
         super.processCookMake(level, maid, be, manager);
+    }
+
+    @Override
+    public void tryInsertItem(ServerLevel serverLevel, EntityMaid entityMaid, KettleBlockEntity blockEntity, MaidRecipesManager<KettleRecipe> maidRecipesManager) {
+        if(this.needWater(blockEntity)) return;
+        super.tryInsertItem(serverLevel, entityMaid, blockEntity, maidRecipesManager);
+    }
+
+    @Override
+    public boolean maidShouldMoveTo(ServerLevel level, EntityMaid maid, KettleBlockEntity kettleBlockEntity, MaidRecipesManager<KettleRecipe> manager) {
+        ItemStackHandler inventory = getItemStackHandler(kettleBlockEntity);
+        ItemStack outputStack = inventory.getStackInSlot(getOutputSlot());
+        // 有最终物品
+        if (!outputStack.isEmpty()) {
+            return true;
+        }
+
+        ItemStack mealStack = getBeInvMealStack(kettleBlockEntity, inventory);
+        ItemStack container = getFoodContainer(kettleBlockEntity);
+        boolean hasOutputAdditionItem = manager.hasOutputAdditionItem(container);
+        // 有待取出物品和对应的容器
+        if (!mealStack.isEmpty() && hasOutputAdditionItem) {
+            return true;
+        }
+
+        boolean heated = isHeated(kettleBlockEntity);
+        Optional<KettleRecipe> recipe = getMatchingRecipe(kettleBlockEntity, new RecipeWrapper(inventory));
+        // 现在是否可以做饭（厨锅有没有正在做饭）
+        boolean b = recipe.isPresent() && canCook(kettleBlockEntity, recipe.get());
+        List<Pair<List<Integer>, List<List<ItemStack>>>> recipesIngredients = manager.getRecipesIngredients();
+        if (!b && !recipesIngredients.isEmpty() && heated && (!needWater(kettleBlockEntity) || findMaidHasWaterResource(maid, kettleBlockEntity) != -1) && mealStack.isEmpty()) {
+            return true;
+        }
+
+        // 能做饭现在和有输入（也就是厨锅现在有物品再里面但是不符合配方
+        if (!b && hasInput(inventory)) {
+            return true;
+        }
+
+        ItemStack containerInputStack = inventory.getStackInSlot(getContainerStackSlot());
+        //当厨锅没有物品，又有杯具在时，就取出杯具
+        if (!hasInput(inventory) && !containerInputStack.isEmpty()) {
+            return true;
+        }
+
+        return false;
     }
 
     public boolean needWater(KettleBlockEntity kettleBlockEntity) {
