8af811dac8bdb6ae553bd2f2937bd3ce4e7006b8
Azumic <775300932@qq.com>
2025-09-22 21:55:27 +0800

    0.3.0.3 错误的配方加载

--- src/main/java/com/github/wallev/maidsoulkitchen/init/ModRecipes.java
+++ src/main/java/com/github/wallev/maidsoulkitchen/init/ModRecipes.java
@@ -1,6 +1,7 @@
 package com.github.wallev.maidsoulkitchen.init;
 
 import com.github.wallev.maidsoulkitchen.MaidsoulKitchen;
+import com.github.wallev.maidsoulkitchen.modclazzchecker.manager.Mods;
 import com.github.wallev.maidsoulkitchen.recipe.itemuse.ItemUseRecipe;
 import com.github.wallev.maidsoulkitchen.recipe.water.ConsumeWaterRecipe;
 import net.minecraft.core.registries.Registries;
@@ -19,20 +20,24 @@ public class ModRecipes {
 
     public static final DeferredRegister<RecipeSerializer<?>> RECIPE_SERIALIZERS = DeferredRegister.create(ForgeRegistries.RECIPE_SERIALIZERS, MaidsoulKitchen.MOD_ID);
 
-    public static RegistryObject<RecipeSerializer<ConsumeWaterRecipe>> CONSUME_WATER_SERIALIZER = RECIPE_SERIALIZERS.register("consume_water", ConsumeWaterRecipe.Serializer::new);
-    public static RegistryObject<RecipeSerializer<ItemUseRecipe>> ITEM_USE_SERIALIZER = RECIPE_SERIALIZERS.register("item_use", ItemUseRecipe.Serializer::new);
+    public static RegistryObject<RecipeSerializer<ConsumeWaterRecipe>> CONSUME_WATER_SERIALIZER;
+    public static RegistryObject<RecipeSerializer<ItemUseRecipe>> ITEM_USE_SERIALIZER;
 
     public static RecipeType<ConsumeWaterRecipe> CONSUME_WATER_RECIPE;
     public static RecipeType<ItemUseRecipe> ITEM_USE_RECIPE;
 
+    static {
+        if (Mods.MAID_STORAGE_MANAGER.versionLoad()) {
+            CONSUME_WATER_SERIALIZER = RECIPE_SERIALIZERS.register("consume_water", ConsumeWaterRecipe.Serializer::new);
+            ITEM_USE_SERIALIZER = RECIPE_SERIALIZERS.register("item_use", ItemUseRecipe.Serializer::new);
+        }
+    }
 
     @SubscribeEvent
     public static void register(RegisterEvent evt) {
-        if (evt.getRegistryKey().equals(Registries.RECIPE_SERIALIZER)) {
+        if (evt.getRegistryKey().equals(Registries.RECIPE_SERIALIZER) && Mods.MAID_STORAGE_MANAGER.versionLoad()) {
             CONSUME_WATER_RECIPE = RecipeType.simple(new ResourceLocation(MaidsoulKitchen.MOD_ID, "consume_water"));
             ITEM_USE_RECIPE = RecipeType.simple(new ResourceLocation(MaidsoulKitchen.MOD_ID, "item_use"));
         }
     }
-
-
 }
